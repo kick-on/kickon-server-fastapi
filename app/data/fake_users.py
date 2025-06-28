1a1726fc9c13282115a772d7a112fa956767c074
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2025-06-28 19:54:19 +0900

    chore: add fake user list for post generation

---
+++ app/data/fake_users.py
@@ -0,0 +1,17 @@
+fake_users = [
+    {
+        "id": "user_01",
+        "nickname": "열정적인 리버풀팬",
+        "team": "리버풀",
+        "style": "fan",
+        "tone": "흥분한 팬처럼 열정적으로 써줘."
+    },
+    {
+        "id": "user_07",
+        "nickname": "분노한 수원팬",
+        "team": "수원 삼성",
+        "style": "angry",
+        "tone": "화난 팬처럼 격하게, 직설적으로 표현해줘."
+    },
+    # ...추가
+]
