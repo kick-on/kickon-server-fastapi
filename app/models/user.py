2d5d7bfb9e08124941dfdbee669e33199467c1ab
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2025-07-13 20:30:26 +0900

    fix: correct table name from 'user' to 'User' in User model

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, String, DateTime
 from app.db.base_class import Base
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "User"
 
     id = Column(String, primary_key=True, index=True)
     nickname = Column(String)
